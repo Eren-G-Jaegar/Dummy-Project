960c8fb06d6f74ac2b4ae19873e8f8cce2322a0e
unknown <sahilhulkhulk@gmail.com>
2026-01-13 14:00:45 +0530

    3rd commit , added a lot of folders and files.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+/*
+
+An example of establishing connection to DB through mongoose
+
+import express from "express";
+const app = express()
+;( async ()=>{
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error)=>{
+      console.log("Error : "+ error);
+      throw error;
+    })
+
+    app.listen(process.env.PORT, ()=>{
+      console.log(`App is listening on port : ${process.env.PORT}`);
+    })
+
+  } catch (error) {
+    console.error(`Error : ${error}`);
+  }
+} )()
+
+  */
