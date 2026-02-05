a2f128036fa04fc773206426d48eb3c12e6d44fc
unknown <sahilhulkhulk@gmail.com>
2026-02-05 15:28:00 +0530

    7th commit, added cloudinary integration with multer for all types of files uploading.

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,65 +1,90 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-
-const userSchema = new Schema( 
+const userSchema = new Schema(
   {
-    userName : {
-      type : String,
-      required : true,
-      unique : true,
-      lowercase : true,
-      trim : true,
-      index : true
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, //Used for searching although it increases complexity
     },
-    email : {
-      type : String,
-      required : true,
-      unique : true,
-      lowercase : true,
-      trim : true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
-    fullName : {
-      type : String,
-      required : true,
-      trim : true,
-      index : true
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
-    avatar : {
-      type : String,         //Cloudinary url
-      required : true
+    avatar: {
+      type: String, //Cloudinary url
+      required: true,
     },
-    coverImage : {
-      type : String,         //Cloudinary url
+    coverImage: {
+      type: String, //Cloudinary url
     },
-    watchHistory : [
+    watchHistory: [
       {
-        type : Schema.Types.ObjectId,
-        ref : "Video"
-      }
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
     ],
-    passWord : {
-      type : String,
-      required : [true , 'Password is required']
+    passWord: {
+      type: String,
+      required: [true, "Password is required"],
     },
-    refreshToken : {
-      type : String
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("passWord")) return next(); //If pwd field isn't modified then there is no need to hash pwd
+
+  this.passWord = await bcrypt.hash(this.passWord, 10);
+  next();
+});
+
+userSchema.methods.isPassWordCorrect = async function (passWord) {
+  //used to decrypt and compare pwds to verify login
+  return await bcrypt.compare(passWord, this.passWord); //it will return either true or false
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
     }
-  } , {timestamps : true} );
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  );
+};
 
-  userSchema.pre("save", async function(next){
-    this.passWord = bcrypt.hash(this.passWord , 10);
-    next();
-  } )
-
-  userSchema.methods.isPassWordCorrect = async function(passWord){
-    return await bcrypt.compare(passWord,this.passWord);
-  }
-
-
-export const User = mongoose.model("User", userSchema );
-
-
-
-
-
+export const User = mongoose.model("User", userSchema);
